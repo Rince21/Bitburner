cd944ab8049b040983c74db27bdc8c7da10f638a
Rince21 <peterpanski42@web.de>
2024-07-24 13:45:13 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,15 +1,5 @@
 /** @param {NS} ns */
-export async function main(ns) {
-
-  //ns.tprint(getNodes())
-  //ns.tprint(getPaths())
-  //ns.tprint(crackServers())
-  //ns.tprint(getRootedServers())
-  //ns.tprint(getWorkingServers())
-  //ns.tprint(getHackableServers())
-ns.tprint(getTargetServers())
-
-  function getNodes() {
+  function getNodes(ns) {
     let nodes = ["home"]
     for (let node of nodes) {
       for (let neigh of ns.scan(node)) {
@@ -21,7 +11,7 @@ ns.tprint(getTargetServers())
     return nodes
   }
 
-  function getPaths() {
+  function getPaths(ns) {
     let paths = { "home": [] }
     let nodes = ["home"]
     for (let node of nodes) {
@@ -37,15 +27,15 @@ ns.tprint(getTargetServers())
     return paths
   }
 
-  function crackServers(servers) {
-    //let cracks = ["bruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe"]
+  function crackServers(ns) {
+    let cracks = ["bruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe"]
     let numOfCracks = 0
     for (let crack of cracks) {
       if (ns.ls("home", crack)) {
         numOfCracks += 1
 
       }
-      for (let server of getNodes()) {
+      for (let server of getNodes(ns)) {
         try { ns.brutessh(server) } catch { }
         try { ns.ftpcrack(server) } catch { }
         try { ns.relaysmtp(server) } catch { }
@@ -57,29 +47,43 @@ ns.tprint(getTargetServers())
     return(numOfCracks)
   }
 
-  function getRootedServers() {
-    return servers.filter((server)=>ns.hasRootAccess(server))
+export  function getRootedServers(ns) {
+    crackServers(ns)
+    return getNodes(ns).filter((server)=>ns.hasRootAccess(server))
   }
 
 
-  function getWorkingServers(){
-    return getRootedServers().filter((server)=>server!="home"&& !server.startsWith("hacknet"))
+export  function getWorkingServers(ns){
+    return getRootedServers(ns).filter((server)=>server!="home"&& !server.startsWith("hacknet"))
   }
 
-  function getHackableServers() {
-    let servers = getNodes()
+export  function getHackableServers(ns) {
+    crackServers(ns)
+    let servers = getNodes(ns)
     servers = servers.filter((server)=>ns.getHackingLevel() >= ns.getServerRequiredHackingLevel(server))
     servers = servers.filter((server)=>server != "home" && !server.startsWith("hacknet") && !server.startsWith("pserv"))
     servers = servers.filter((server)=>ns.getServerMaxMoney(server)>0)
     return servers
   }
 
-  function getTargetServers () {
-    let servers = getHackableServers()
+export  function getTargetServers (ns) {
+    let servers = getHackableServers(ns)
     servers = servers.filter((server)=>ns.getHackingLevel()>=ns.getServerRequiredHackingLevel(server)*2)
     servers = servers.map((server)=>[server,ns.getServerRequiredHackingLevel(server)])
     servers.sort((a,b)=>b[1]-a[1])
     return servers
   }
+
+export async function main(ns) {
+
+  //ns.tprint(getNodes())
+  //ns.tprint(getPaths())
+  //ns.tprint(crackServers())
+  //ns.tprint(getRootedServers())
+  ns.tprint(getWorkingServers(ns))
+  //ns.tprint(getHackableServers())
+//ns.tprint(getTargetServers())
+
+
 
 }
