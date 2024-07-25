7c289f084ef70446a7c783b19cc5c34bf5eee225
Rince21 <peterpanski42@web.de>
2024-07-25 12:29:08 +0200

    Create buy.js

---
+++ buy.js
@@ -0,0 +1,16 @@
+/** @param {NS} ns */
+export async function main(ns) {
+ns.singularity.travelToCity("Aevum")
+ns.singularity.goToLocation("ecorp")
+ns.singularity.purchaseTor()
+ns.singularity.purchaseProgram("BruteSSH.exe");
+ns.singularity.purchaseProgram("FTPCrack.exe");
+ns.singularity.purchaseProgram("relaySMTP.exe");
+ns.singularity.purchaseProgram("HTTPWorm.exe");
+ns.singularity.purchaseProgram("SQLInject.exe");
+ns.singularity.purchaseProgram("ServerProfiler.exe");
+ns.singularity.purchaseProgram("DeepscanV1.exe");
+ns.singularity.purchaseProgram("DeepscanV2.exe");
+ns.singularity.purchaseProgram("AutoLink.exe");
+ns.singularity.purchaseProgram("Formulas.exe");
+}
